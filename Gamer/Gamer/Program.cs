49906638d63b66dc3c01dfd5c262c88756e2d6b0
JonasNJensen <102585691+JonasNJensen@users.noreply.github.com>
2023-01-25 15:00:58 +0100

    Create alarm

--- Gamer/Gamer/Program.cs
+++ Gamer/Gamer/Program.cs
@@ -5,6 +5,9 @@ Console.Clear();
 Thread clockThread = new Thread(UpdateClock);
 clockThread.Start();
 
+Thread clockThread = new Thread(UpdateClock);
+clockThread.Start()
+
 static void UpdateClock()
 {
 	while (true)
@@ -14,4 +17,16 @@ static void UpdateClock()
 
 		Thread.Sleep(1000);
 	}
+}
+
+static void CreateAlarm()
+{
+	System.Console.WriteLine("enter the time you wish to set an alarm for as HH:mm e.g. 10:30");
+	string alarm = System.Console.readLine();
+
+	while(alarm != DateTime.Now.ToString("HH:mm"));
+	{
+		Thread.Sleep(5000);
+	}
+	System.console.beep(800, 200);
 }
