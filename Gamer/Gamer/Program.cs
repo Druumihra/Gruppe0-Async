67ac9dd9990b3f26c55e0b5311a7b3fd3156604a
ReimarPB <reimarpb@gmail.com>
2023-01-25 15:16:30 +0100

    Fix alarm

--- Gamer/Gamer/Program.cs
+++ Gamer/Gamer/Program.cs
@@ -5,8 +5,8 @@ Console.Clear();
 Thread clockThread = new Thread(UpdateClock);
 clockThread.Start();
 
-Thread clockThread = new Thread(UpdateClock);
-clockThread.Start()
+Thread alarmThread = new Thread(CreateAlarm);
+alarmThread.Start();
 
 static void UpdateClock()
 {
@@ -21,12 +21,13 @@ static void UpdateClock()
 
 static void CreateAlarm()
 {
-	System.Console.WriteLine("enter the time you wish to set an alarm for as HH:mm e.g. 10:30");
-	string alarm = System.Console.readLine();
+	Console.SetCursorPosition(0, 2);
+	System.Console.WriteLine("enter the time you wish to set an alarm for as HH.mm e.g. 10.30");
+	string alarm = System.Console.ReadLine();
 
-	while(alarm != DateTime.Now.ToString("HH:mm"));
+	while(alarm != DateTime.Now.ToString("HH:mm"))
 	{
 		Thread.Sleep(5000);
 	}
-	System.console.beep(800, 200);
+	System.Console.Beep(800, 200);
 }
